ebe3802369820c85329e83c9c965971b15647e56
ben1one <ben1one@users.noreply.github.com>
2025-05-05 15:12:35 +0000

    update 8e6040da2b4a62780700084db1716aaaae5b8b88

---
+++ precache-manifest.971a1e7d9b1917738edab8f89d0368ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b573e81083279e39de6d3271480f35fa",
+    "url": "/jp-verb/index.html"
+  },
+  {
+    "revision": "6674ec0df2af0a23e42f",
+    "url": "/jp-verb/static/js/2.37d298d3.chunk.js"
+  },
+  {
+    "revision": "a02116024b3bb15a81f43342a831712c",
+    "url": "/jp-verb/static/js/2.37d298d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e42ffa13138703b9bd4",
+    "url": "/jp-verb/static/js/3.7ec1b03a.chunk.js"
+  },
+  {
+    "revision": "0376a9313b278e37cf5e",
+    "url": "/jp-verb/static/js/4.ea782fe7.chunk.js"
+  },
+  {
+    "revision": "05708ed176005b0a5f13",
+    "url": "/jp-verb/static/js/main.844090d6.chunk.js"
+  },
+  {
+    "revision": "8d4effbb85cf51c89b75",
+    "url": "/jp-verb/static/js/runtime-main.eea8a0d3.js"
+  },
+  {
+    "revision": "de265e253427fbfadaa36177c3bd6e94",
+    "url": "/jp-verb/static/media/mini.de265e25.jpg"
+  },
+  {
+    "revision": "d8f18b9d64b529053496d645be8425c6",
+    "url": "/jp-verb/static/media/qrcode.d8f18b9d.png"
+  }
+]);

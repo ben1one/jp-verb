8e6040da2b4a62780700084db1716aaaae5b8b88
Ben Suen <ben1one@gmail.com>
2025-05-05 23:11:01 +0800

    update

---
+++ docs/precache-manifest.cb07f1debae6882fa341836ce282395c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5d23a467f52aa292122f8aeb9fcba8",
+    "url": "/jp-verb/index.html"
+  },
+  {
+    "revision": "56c414681368bd862c55",
+    "url": "/jp-verb/static/js/2.cb880a47.chunk.js"
+  },
+  {
+    "revision": "b6842a5597b7f1eec296067f4813c347",
+    "url": "/jp-verb/static/js/2.cb880a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e42ffa13138703b9bd4",
+    "url": "/jp-verb/static/js/3.7ec1b03a.chunk.js"
+  },
+  {
+    "revision": "0376a9313b278e37cf5e",
+    "url": "/jp-verb/static/js/4.ea782fe7.chunk.js"
+  },
+  {
+    "revision": "05708ed176005b0a5f13",
+    "url": "/jp-verb/static/js/main.844090d6.chunk.js"
+  },
+  {
+    "revision": "8d4effbb85cf51c89b75",
+    "url": "/jp-verb/static/js/runtime-main.eea8a0d3.js"
+  },
+  {
+    "revision": "de265e253427fbfadaa36177c3bd6e94",
+    "url": "/jp-verb/static/media/mini.de265e25.jpg"
+  },
+  {
+    "revision": "d8f18b9d64b529053496d645be8425c6",
+    "url": "/jp-verb/static/media/qrcode.d8f18b9d.png"
+  }
+]);

9a208c71e565594d99a7fcb4197cbc7e157373bb
Ben Suen <ben1one@gmail.com>
2025-05-10 14:21:14 +0800

    update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jp-verb/precache-manifest.043983f1129522f83b711234852305e8.js"
+  "/jp-verb/precache-manifest.96e9cac82ec20c73bc9bd58282abac59.js"
 );
 
 self.addEventListener('message', (event) => {
